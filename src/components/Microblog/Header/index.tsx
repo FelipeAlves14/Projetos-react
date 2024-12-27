1aa98b6aba0cd50c0628a77a20c47c8fbba3c254
Matheus Alves <129554334+MatheusADV@users.noreply.github.com>
2024-12-27 08:28:46 -0300

    feat: adicionando link para site informativo

--- src/components/Microblog/Header/index.tsx
+++ src/components/Microblog/Header/index.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import useUsersStore from "../usersStore";
 
-export default function Header() {
+export default function Header(): JSX.Element {
   const { user } = useUsersStore();
   return (
     <header className="br-header container-fluid compact mb-3">
@@ -9,93 +9,16 @@ export default function Header() {
         <div className="header-top">
           <div className="header-actions">
             <div className="header-links dropdown">
-              <button
-                className="br-button circle small"
-                type="button"
-                data-toggle="dropdown"
-                aria-label="Abrir Acesso Rápido"
-              >
-                <i className="fas fa-ellipsis-v" aria-hidden="true"></i>
-              </button>
               <div className="br-list">
-                <div className="header">
-                  <div className="title">Acesso Rápido</div>
-                </div>
-                <a className="br-item" href="javascript:void(0)">
+                <Link className="br-item" target="blank" to="https://sproutsocial.com/pt/glossary/microblog/">
                   O que é um Microblog?
-                </a>
+                </Link>
                 <a className="br-item" href="javascript:void(0)">
                   Sobre nós
                 </a>
               </div>
             </div>
             <span className="br-divider vertical mx-half mx-sm-1"></span>
-            <div className="header-functions dropdown">
-              <button
-                className="br-button circle small"
-                type="button"
-                data-toggle="dropdown"
-                aria-label="Abrir Funcionalidades do Sistema"
-              >
-                <i className="fas fa-th" aria-hidden="true"></i>
-              </button>
-              <div className="br-list">
-                <div className="header">
-                  <div className="title">Funcionalidades do Sistema</div>
-                </div>
-                <div className="br-item">
-                  <button
-                    className="br-button circle small"
-                    type="button"
-                    aria-label="Funcionalidade 1"
-                  >
-                    <i className="fas fa-chart-bar" aria-hidden="true"></i>
-                    <span className="text">Funcionalidade 1</span>
-                  </button>
-                </div>
-                <div className="br-item">
-                  <button
-                    className="br-button circle small"
-                    type="button"
-                    aria-label="Funcionalidade 2"
-                  >
-                    <i className="fas fa-headset" aria-hidden="true"></i>
-                    <span className="text">Funcionalidade 2</span>
-                  </button>
-                </div>
-                <div className="br-item">
-                  <button
-                    className="br-button circle small"
-                    type="button"
-                    aria-label="Funcionalidade 3"
-                  >
-                    <i className="fas fa-comment" aria-hidden="true"></i>
-                    <span className="text">Funcionalidade 3</span>
-                  </button>
-                </div>
-                <div className="br-item">
-                  <button
-                    className="br-button circle small"
-                    type="button"
-                    aria-label="Funcionalidade 4"
-                  >
-                    <i className="fas fa-adjust" aria-hidden="true"></i>
-                    <span className="text">Funcionalidade 4</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="header-search-trigger">
-              <button
-                className="br-button circle"
-                type="button"
-                aria-label="Abrir Busca"
-                data-toggle="search"
-                data-target=".header-search"
-              >
-                <i className="fas fa-search" aria-hidden="true"></i>
-              </button>
-            </div>
             <div className="header-login">
               <div className="header-sign-in">
                 <Link className="br-sign-in small" to={"/Microblog/login"}>
