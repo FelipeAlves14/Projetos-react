a3502f9e9cda291b69be46396907725706364475
Matheus Alves <129554334+MatheusADV@users.noreply.github.com>
2024-12-27 14:49:33 -0300

    fix: consertando bug de requisições infinitas

--- src/components/Microblog/Feed/index.tsx
+++ src/components/Microblog/Feed/index.tsx
@@ -1,33 +1,167 @@
-import { useEffect, useState } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 import axiosRequestor from "../axiosRequestor";
-import FormularioPublicacao from "../FormularioPublicacao";
+import { useEffect, useState } from "react";
 import Header from "../Header";
-import Publicacao from "../Publicacao";
-import { PublicacaoProps } from "../Publicacao";
+import Publicacao, { PublicacaoProps } from "../Publicacao";
+import { Link, NavigateFunction, useNavigate } from "react-router-dom";
+
+export interface PublicacaoFormProps {
+  titulo: string;
+  descricao: string;
+  imagem?: File | string;
+}
 
 export default function Feed(): JSX.Element {
   const [pubs, setPubs] = useState<PublicacaoProps[]>([]);
+  const [novaPub, setNovaPub] = useState<boolean>(false);
+  const schema = yup.object().shape({
+    titulo: yup
+      .string()
+      .required("Preencha todos os campos obrigatórios")
+      .max(200, "Este campo excedeu o limite de caracteres"),
+    descricao: yup
+      .string()
+      .required("Preencha todos os campos obrigatórios")
+      .max(1200, "Este campo excedeu o limite de caracteres"),
+    imagem: yup.mixed<File>(),
+  });
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const navigate: NavigateFunction = useNavigate();
+  async function submitPub(pub: PublicacaoFormProps): Promise<void> {
+    try {
+      const data: PublicacaoFormProps = {
+        titulo: pub.titulo,
+        descricao: pub.descricao,
+        imagem: pub.imagem[0] ?? "",
+      };
+      await axiosRequestor.post("publicacao/", data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      reset();
+      setNovaPub(!novaPub);
+    } catch (err) {
+      console.error(err);
+      navigate("/Microblog/login");
+    }
+  }
+  const fetchPublicacoes = async (): Promise<void> => {
+    const publicacoes = await axiosRequestor
+      .get("publicacao/", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => response.data.results);
+    setPubs(publicacoes);
+  };
+
   useEffect(() => {
-    const fetchPublicacoes = async (): Promise<void> => {
-      const publicacoes = await axiosRequestor
-        .get("publicacao/", {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-        .then((response) => response.data.results);
-      if (publicacoes !== pubs) setPubs(publicacoes);
-    };
     fetchPublicacoes();
   }, []);
+  useEffect(() => {
+    fetchPublicacoes();
+  }, [novaPub]);
   return (
     <>
       <Header />
       <div className="d-flex">
         <div className="container m-1 mr-6 d-flex flex-column">
-          <FormularioPublicacao />
+          <form
+            className="col-4 shadow-sm pt-3"
+            style={{ position: "fixed" }}
+            onSubmit={handleSubmit(submitPub)}
+            encType="multipart/form-data"
+          >
+            <div className="text-weight-semi-bold col-10 text-center ml-5 mb-3 text-up-03">
+              Faça uma publicação
+            </div>
+            <div className="col-10 mb-3 ml-4">
+              <div className="text-left pl-4 br-input">
+                <label htmlFor="input-default">Título</label>
+                <input
+                  id="input-default"
+                  type="text"
+                  placeholder="Ex.: Microblog da PNP"
+                  {...register("titulo")}
+                />
+                {errors.titulo?.message && (
+                  <span className="feedback danger" role="alert" id="danger">
+                    <i className="fas fa-times-circle" aria-hidden="true"></i>
+                    {errors.titulo?.message}
+                  </span>
+                )}
+              </div>
+            </div>
+            <div className="text-left col-10 pl-4 mb-3 ml-4 br-textarea">
+              <label htmlFor="textarea-id1">Descrição</label>
+              <textarea
+                id="textarea-id1"
+                placeholder="Ex.: Muito bom o Microblog..."
+                {...register("descricao")}
+              ></textarea>
+              {errors.descricao?.message && (
+                <span className="feedback danger" role="alert" id="danger">
+                  <i className="fas fa-times-circle" aria-hidden="true"></i>
+                  {errors.descricao?.message}
+                </span>
+              )}
+            </div>
+            <div className="text-left mb-3 ml-4 pl-4">
+              <label className="upload-label" htmlFor="multiple-files">
+                <span>Envio de arquivos </span>
+              </label>
+              <input
+                className="upload-input"
+                id="imagem"
+                type="file"
+                accept="image/*"
+                aria-hidden={false}
+                aria-label="enviar arquivo"
+                {...register("imagem")}
+              />
+              <div className="upload-list mb-5"></div>
+            </div>
+            <div className="text-right mr-6">
+              <button className="br-button primary mr-6 mb-4" type="submit">
+                <i className="fas fa-share" aria-hidden="true"></i>
+                Publicar
+              </button>
+            </div>
+            <div
+              className="col-4 shadow-sm text-right p-3 mt-3"
+              style={{ position: "fixed" }}
+            >
+              <div className="m-3 text-left">
+                <h3>Quer descobrir em que foi inspirado o nosso microblog?</h3>
+                <h3 className="mb-6 text-left">
+                  E nossos padrões de design, tem curiosidade?
+                </h3>
+              </div>
+              <div className="br-magic-button">
+                <Link
+                  to="https://www.gov.br/ds/home"
+                  target="blank"
+                  className="br-button"
+                  type="button"
+                >
+                  Descubra aqui
+                </Link>
+              </div>
+            </div>
+          </form>
         </div>
-        <div className="container p-0">
+        <div className="mt-1 p-0">
           {pubs.length !== 0 ? (
             pubs.map((pub, index) => (
               <Publicacao
--- src/components/Microblog/Publicacao/index.tsx
+++ src/components/Microblog/Publicacao/index.tsx
@@ -4,6 +4,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import axiosRequestor from "../axiosRequestor";
 import { NavigateFunction, useNavigate } from "react-router-dom";
+import Comentario, { ComentarioProps } from "../Comentario";
 
 export interface PublicacaoProps {
   id: number;
@@ -18,19 +19,12 @@ export interface UserProps {
   id?: number;
   username: string;
   nome?: string;
+  ultimo_login: Date;
 }
 
 export interface ComentarioFormProps {
   publicacao?: number;
   mensagem: string;
-}
-
-export interface ComentarioProps {
-  id: number;
-  autor: UserProps;
-  publicacao: number;
-  mensagem: string;
-  publicado_em: string;
 }
 
 export default function Publicacao(props: PublicacaoProps): JSX.Element {
@@ -49,7 +43,12 @@ export default function Publicacao(props: PublicacaoProps): JSX.Element {
     resolver: yupResolver(schema),
   });
   const { id, autor, publicado_em, descricao, imagem, titulo } = props;
+  const data_publicacao = new Date(publicado_em);
+  const dia = (data_publicacao.getDate()).toString().padStart(2, "0");
+  const mes = (data_publicacao.getMonth() + 1).toString().padStart(2, "0");
+  const ano = data_publicacao.getFullYear().toString();
   const [comentarios, setComentarios] = useState<boolean>(false);
+  const [novoComentario, setNovoComentario] = useState<boolean>(false);
   const [listaComentarios, setListaComentarios] = useState<ComentarioProps[]>(
     []
   );
@@ -66,38 +65,43 @@ export default function Publicacao(props: PublicacaoProps): JSX.Element {
           "Content-Type": "application/json",
         },
       });
-      window.location.reload();
+      setNovoComentario(!novoComentario);
     } catch (err) {
       console.error(err);
       navigate("/Microblog/login");
     }
     reset();
   }
+  const fetchComentarios = async (): Promise<void> => {
+    const comentarios = await axiosRequestor
+      .get(`publicacao/${id}/comentarios/`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => response.data.results);
+    setListaComentarios(comentarios);
+  };
 
   useEffect(() => {
-    const fetchComentarios = async (): Promise<void> => {
-      const comentarios = await axiosRequestor
-        .get(`publicacao/${id}/comentarios/`, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-        .then((response) => response.data.results);
-      setListaComentarios(comentarios);
-    };
     fetchComentarios();
   }, []);
+  useEffect(() => {
+    fetchComentarios();
+  }, [novoComentario]);
 
   return (
-    <div className="col-10 p-0">
-      <div className="br-card">
+    <div className="col-12">
+      <div className="br-card mr-6">
         <div className="card-header">
           <div className="ml-3 d-flex text-center justify-content-end">
             <div className="text-weight-semi-bold text-up-02">
-              {autor ? autor?.username : "Autor desconhecido"}
+              {autor.username}
             </div>
             <div className="ml-auto">
-              <div>{publicado_em}</div>
+              <div>
+                {dia}/{mes}/{ano}
+              </div>
             </div>
           </div>
           <h6 className="text-left ml-3">{titulo}</h6>
@@ -107,67 +111,70 @@ export default function Publicacao(props: PublicacaoProps): JSX.Element {
           className={`card-content text-left ${imagem && "d-flex"}`}
           tabIndex={0}
         >
-          <p className="ml-3">{descricao}</p>
+          <p className="mr-3">{descricao}</p>
           {imagem && (
             <img src={imagem} alt="imagem-pub" width={200} height={200} />
           )}
         </div>
         <div className="d-flex justify-content-end">
-          <a
-            className="br-button m-1"
+          <button
+            className="br-button mx-3 my-1"
             onClick={() => {
               setComentarios(!comentarios);
             }}
           >
-            Ver comentários
-          </a>
+            {comentarios ? (
+              <span>
+                <i className="fas fa-eye-slash" aria-hidden="true"></i>Ocultar
+                comentários
+              </span>
+            ) : (
+              <span>
+                <i className="fas fa-eye" aria-hidden="true"></i>Ver comentários
+              </span>
+            )}
+          </button>
         </div>
+        <span className="br-divider light-mode mb-3 mx-3"></span>
+        <form onSubmit={handleSubmit(submitComment)}>
+          <div className="flex-column text-left">
+            <div className="col-12">
+              <div className="br-input mx-3">
+                <input
+                  id="input-default"
+                  type="text"
+                  placeholder="Ex.: Adorei esta publicação..."
+                  {...register("mensagem")}
+                />
+              </div>
+              {errors.mensagem?.message && (
+                <span className="feedback danger mx-3" role="alert" id="danger">
+                  <i className="fas fa-times-circle" aria-hidden="true"></i>
+                  {errors.mensagem?.message}
+                </span>
+              )}
+            </div>
+          </div>
+          <div className="text-right">
+            <button className="br-button m-3 secondary" type="submit">
+              <i className="fas fa-share" aria-hidden="true"></i>
+              Comentar
+            </button>
+          </div>
+        </form>
         {comentarios && (
           <>
             <span className="br-divider light-mode mb-3 mx-3"></span>
             <div className="card-footer d-flex flex-column">
-              <form onSubmit={handleSubmit(submitComment)}>
-                <div className="flex-column text-left">
-                  <button className="br-button secondary mb-2" type="submit">
-                    Comentar
-                  </button>
-                  <div className="col-12">
-                    <div className="br-input">
-                      <input
-                        id="input-default"
-                        type="text"
-                        placeholder="Escreva aqui seu comentário..."
-                        {...register("mensagem")}
-                      />
-                    </div>
-                    {errors.mensagem?.message && (
-                      <span
-                        className="feedback danger"
-                        role="alert"
-                        id="danger"
-                      >
-                        <i
-                          className="fas fa-times-circle"
-                          aria-hidden="true"
-                        ></i>
-                        {errors.mensagem?.message}
-                      </span>
-                    )}
-                  </div>
-                </div>
-              </form>
               <div className="d-flex overflow-auto flex-column">
                 {listaComentarios.map(
                   (comentario: ComentarioProps, index: number) => (
-                    <div key={index} className="text-left w-fixed">
-                      <div className="d-flex justify-content-between">
-                        <legend className="text-up-02">{comentario.autor.username}</legend>
-                        <legend className="text-weight-light">
-                          {comentario.publicado_em.substring(0, 10)}
-                        </legend>
-                      </div>
-                      <p>{comentario.mensagem}</p>
-                    </div>
+                    <Comentario
+                      key={index}
+                      autor={comentario.autor}
+                      publicado_em={comentario.publicado_em}
+                      mensagem={comentario.mensagem}
+                    />
                   )
                 )}
               </div>
