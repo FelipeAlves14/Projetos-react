67d1c505f09ba842bfcbecffd32d742b39aa078e
FelipeAlves14 <favasconcelos09@gmail.com>
2025-01-13 15:36:41 -0300

    fix: removendo duplicidade do header

--- src/components/Microblog/index.tsx
+++ src/components/Microblog/index.tsx
@@ -1,10 +1,8 @@
-import Header from "./Header";
 import Login from "./Login";
 
 export default function Microblog(): JSX.Element {
   return (
     <>
-      <Header />
       <Login />
     </>
   );
